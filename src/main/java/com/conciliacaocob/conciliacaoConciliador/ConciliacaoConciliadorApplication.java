53dee37693d0368b1f45d1134de0b5e712f2452f
Diego Rangel <diego.freire@conductor.com.br>
2021-04-19 14:07:32 -0300

    Subindo projeto para o repositorio

---
+++ src/main/java/com/conciliacaocob/conciliacaoConciliador/ConciliacaoConciliadorApplication.java
@@ -0,0 +1,13 @@
+package com.conciliacaocob.conciliacaoConciliador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConciliacaoConciliadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConciliacaoConciliadorApplication.class, args);
+	}
+
+}
